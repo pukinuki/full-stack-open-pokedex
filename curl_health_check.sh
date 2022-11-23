cf6c6f5f27d7647c0204b192d4ebb916bbf9c8df
Pukinuki <fluqueo@gmail.com>
2022-11-23 16:27:35 +0100

    11.12 custom health check v4

--- curl_health_check.sh
+++ curl_health_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#req=$(curl -s https://pokedex-pukinuki.fly.dev/health)
-req=$(curl -s http://127.0.0.1:5000/health)
+req=$(curl -s https://pokedex-pukinuki.fly.dev/health)
+#req=$(curl -s http://127.0.0.1:5000/health)
 
-if [ $req -eq "ok" ]; then
+if [[ $req = "ok" ]]; then
     echo "Health check: ok"
     exit 0
 fi
